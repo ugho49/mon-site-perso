c55f14624e91b98097064efbe5c1b2bb2cc85e6c
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-10 00:25:36 +0100

    ajout compétences

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
-<?php include '../layouts/header.php'; ?>
 <?php
-//Constantes
+include '../layouts/header.php';
+$pdo = require '../src/pdo.php';
 $color = "grey darken-1";
 $color_btn = "grey lighten-1";
 ?>
@@ -21,7 +21,8 @@ $color_btn = "grey lighten-1";
 <!-- DEBUT Partie Avatar -->
 <div class="section white">
   <div class="row container">
-    <h2 class="header">A propos de moi</h2>
+    <h2 class="title_section">A propos de moi</h2>
+    <span class="title_border"></span>
     <p class="grey-text text-darken-3 lighten-3">
       Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
     </p>
@@ -30,28 +31,14 @@ $color_btn = "grey lighten-1";
 <!-- FIN Partie Avatar -->
 
 <div class="parallax-container">
-  <div class="parallax"><img src="img/image2.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
-</div>
-
-<!-- DEBUT Partie Compétence -->
-<div class="section white">
-  <div class="row container">
-    <h2 class="header">Mes compétences</h2>
-    <p class="grey-text text-darken-3 lighten-3">
-      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
-    </p>
-  </div>
-</div>
-<!-- FIN Partie Compétence -->
-
-<div class="parallax-container">
   <div class="parallax"><img src="img/image3.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
 </div>
 
 <!-- DEBUT Partie Expérience -->
 <div class="section white">
   <div class="row container">
-    <h2 class="header">Mes expériences</h2>
+    <h2 class="title_section">Mes expériences</h2>
+    <span class="title_border"></span>
     <p class="grey-text text-darken-3 lighten-3">
       Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
     </p>
@@ -60,13 +47,59 @@ $color_btn = "grey lighten-1";
 <!-- FIN Partie Expérience -->
 
 <div class="parallax-container">
+  <div class="parallax"><img src="img/image2.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
+</div>
+
+<!-- DEBUT Partie Compétence -->
+<div class="section white">
+  <div class="row container">
+    <h2 class="title_section">Mes compétences</h2>
+    <span class="title_border"></span>
+    <?php
+      $requete = $pdo->prepare('SELECT * FROM type_skills ORDER BY id DESC');
+      $requete->execute();
+      $types = $requete->fetchAll();
+    ?>
+
+    <?php foreach ($types as $type): ?>
+
+      <?php if ($type->id == 1): ?>
+          <div class="col l12 s12">
+      <?php else: ?>
+          <div class="col l6 s12">
+      <?php endif; ?>
+          <h4><?= $type->libelle; ?></h4>
+          <?php
+            $requete = $pdo->prepare('SELECT * FROM skills WHERE type_skill_id = ?');
+            $requete->execute([$type->id]);
+            $skills = $requete->fetchAll();
+            $cpt = 0;
+          ?>
+          <?php foreach ($skills as $skill): ?>
+            <span class="title-progress"><?= $skill->libelle; ?></span>
+            <div class="row">
+              <div class="col s12">
+                <div class="progress">
+                    <div class="determinate" style="width: <?= $skill->percentage; ?>%; background-color: <?= $skill->color_hexa; ?>"></div>
+                </div>
+              </div>
+            </div>
+          <?php endforeach; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
+<!-- FIN Partie Compétence -->
+
+<div class="parallax-container">
   <div class="parallax"><img src="img/image4.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
 </div>
 
 <!-- DEBUT Partie Contact -->
 <div class="section white">
   <div class="row container">
-    <h2 class="header">Me Contacter</h2>
+    <h2 class="title_section">Me Contacter</h2>
+    <span class="title_border"></span>
     <p class="grey-text text-darken-3 lighten-3">
       Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
     </p>
--- src/pdo.php
+++ src/pdo.php
@@ -2,3 +2,5 @@
 
 $pdo = new PDO('mysql:host=localhost;dbname=ughostephan;charset=utf8', 'ughostephan', 'MK7vAEXCqhEmAUzt', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+return $pdo;
