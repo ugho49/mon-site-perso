1c8cea884ac7d379c23aadc9efcb1191118dd42b
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-09 17:20:58 +0100

    structure fini

--- layouts/header.php
+++ layouts/header.php
@@ -1,3 +1,31 @@
-<p>
-  header
-</p>
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/app.css">
+    <!-- Let browser know website is optimized for mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- Other -->
+    <title>Ugho Stephan</title>
+    <meta name="language" content="fr">
+    <meta name="keywords" content="sport, fitness, musculation, angers, perte, de, poid, developpement, masse, musculaire, sauna">
+    <meta name="author" content="Ugho STEPHAN">
+    <meta name="publisher" content="Ugho STEPHAN">
+    <meta name="copyright" content="Ugho STEPHAN">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/icon">
+
+    <meta charset="utf-8" />
+
+  	<meta http-equiv="cache-control" content="max-age=0" />
+  	<meta http-equiv="cache-control" content="no-cache" />
+  	<meta http-equiv="expires" content="0" />
+  	<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+  	<meta http-equiv="pragma" content="no-cache" />
+  </head>
+
+  <body>
--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,88 @@
 <?php include '../layouts/header.php'; ?>
-<h1>Bienvenue :-)</h1>
-<h2>Site en cours de construction ...</h2>
+<?php
+//Constantes
+$color = "grey darken-1";
+$color_btn = "grey lighten-1";
+?>
+
+<div class="parallax-container">
+  <div class="parallax">
+    <img src="img/image1.jpg" style="display: block; transform: translate3d(-50%, 172px, 0px);" class="fond_info">
+    <div class="info">
+        <img src="img/avatar.jpg" alt="avatar" class="avatar circle" />
+        <h2>Ugho Stephan</h2>
+        <h3>Developer Web & Mobile </h3>
+    </div>
+  </div>
+</div>
+<div class="section white">
+  <div class="row container">
+    <h2 class="header">A propos de moi</h2>
+    <p class="grey-text text-darken-3 lighten-3">
+      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+    </p>
+  </div>
+</div>
+
+<div class="parallax-container">
+  <div class="parallax"><img src="img/image2.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
+</div>
+
+<div class="section white">
+  <div class="row container">
+    <h2 class="header">Mes compétences</h2>
+    <p class="grey-text text-darken-3 lighten-3">
+      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+    </p>
+  </div>
+</div>
+
+<div class="parallax-container">
+  <div class="parallax"><img src="img/image3.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
+</div>
+
+<div class="section white">
+  <div class="row container">
+    <h2 class="header">Mes expériences</h2>
+    <p class="grey-text text-darken-3 lighten-3">
+      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+    </p>
+  </div>
+</div>
+
+<div class="parallax-container">
+  <div class="parallax"><img src="img/image4.jpg" style="display: block; transform: translate3d(-50%, 85px, 0px);"></div>
+</div>
+
+<div class="section white">
+  <div class="row container">
+    <h2 class="header">Me Contacter</h2>
+    <p class="grey-text text-darken-3 lighten-3">
+      Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+    </p>
+  </div>
+</div>
+
+<footer class="page-footer <?= $color; ?>" style="margin-top:0px;">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text">Aider Materialize à grandir</h5>
+        <p class="grey-text text-lighten-4">We hope you have enjoyed using Materialize! If you feel Materialize has helped you out and want to support the team, send us over a donation! Any amount would help support and continue development on this project and is greatly appreciated.</p>
+        <form id="paypal-donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <button class="btn waves-effect waves-light <?= $color_btn; ?>" type="submit" name="action" alt="PayPal - The safer, easier way to pay online!">Faire un don</button>
+        </form>
+
+      </div>
+      <div class="col l6 s12">
+        <h5 class="white-text">Rejoindre la discussion</h5>
+        <p class="grey-text text-lighten-4">We have a Gitter chat room set up where you can talk directly with us. Come in and discuss new features, future goals, general problems or questions, or anything else you can think of.</p>
+        <a class="btn waves-effect waves-light <?= $color_btn; ?>" target="_blank" href="https://gitter.im/Dogfalo/materialize">Chat</a>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">© 2014-2015 Materialize, All rights reserved. <a class="grey-text text-lighten-4 right" href="https://github.com/Dogfalo/materialize/blob/master/LICENSE">MIT License</a> </div>
+  </div>
+</footer>
 <?php include '../layouts/footer.php'; ?>
