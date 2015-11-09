b91d8ffeeb96ee78cd806b9b20824dce14ca336a
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-09 14:05:29 +0100

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,4 @@
+<?php include '../layouts/header.php'; ?>
+<h1>Bienvenue :-)</h1>
+<h2>Site en cours de construction ...</h2>
+<?php include '../layouts/footer.php'; ?>
---
+++ src/pdo.php
@@ -0,0 +1,4 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=ughostephan;charset=utf8', 'ughostephan', 'MK7vAEXCqhEmAUzt');
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
