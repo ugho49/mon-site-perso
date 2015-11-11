67b5f4114a2816296b0377210685c0be5d6732a8
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-11 15:52:49 +0100

    draggable only for large screen

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
     $('.btn_home').hide();
     checkScroll();
 
+    var isDraggable = $(document).width() > 480 ? true : false;
+
     //Google Maps
     $('#gmap').gmap3({
         marker:{address:"Nantes, France", options:{icon: "img/location1.png"}},
         map:{
             options:{
                 zoom: 14,
-                scrollwheel: false
+                scrollwheel: false,
+                draggable: isDraggable
             }
         }
     });
