cbe3fec0fffe2aa7572350fb49a342d3f3e78280
Ugho STEPHAN <stephan.ugho@gmail.com>
2015-11-17 17:06:15 +0100

    Reorganize footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,27 +1,29 @@
-    <!--Import jQuery before materialize.js-->
+    
+    <!-- jQuery -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <!-- Appear -->
     <script type="text/javascript" src="js/appear.min.js"></script>
+    <!-- Materialize -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js"></script>
+    <!-- Init Materialize -->
+    <script type="text/javascript" src="js/init.min.js"></script>
+    <!-- App -->
+    <script type="text/javascript" src="js/app.min.js"></script>
+    <!-- Google Map -->
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=fr"></script>
     <script type="text/javascript" src="js/gmap3.min.js"></script>
-
-    <!--Compiled and minified JavaScript-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js"></script>
-    <!--Init materialize-->
-    <script type="text/javascript" src="js/init.min.js"></script>
-    <!--Others-->
-    <script type="text/javascript" src="js/app.min.js"></script>
-
-    <script src='https://www.google.com/recaptcha/api.js'></script>
-
+    <!-- Google Recaptcha -->
+    <script type="text/javascript" src='https://www.google.com/recaptcha/api.js'></script>
+    <!-- Google Analytics -->
     <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-48402317-4', 'auto');
-      ga('send', 'pageview');
-
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+          ga('create', 'UA-48402317-4', 'auto');
+          ga('send', 'pageview');
     </script>
+    
   </body>
 </html>
