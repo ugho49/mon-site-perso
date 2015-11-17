49307b50019f2b4995a3a824da31887b702c04a0
Ugho STEPHAN <stephan.ugho@gmail.com>
2015-11-17 17:00:58 +0100

    Reorganize header.php

--- layouts/header.php
+++ layouts/header.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css">
-
-    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/app.min.css">
-    <link rel="stylesheet" href="css/animate.min.css">
-    <!-- Let browser know website is optimized for mobile -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <!-- Other -->
+   
+    <!-- Important Header -->
     <title>Ugho Stephan</title>
+    <meta charset="utf-8" />
     <meta name="language" content="fr">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/icon">
+    <link rel="alternate" href="http://www.ugho-stephan.fr" hreflang="fr" />
     <meta name="keywords" content="ugho stephan, developpeur, etudiant, mobile, web">
     <meta name="description" content="Site personnel de Ugho STEPHAN étudiant en informatique ...">
     <meta name="author" content="Ugho STEPHAN">
     <meta name="publisher" content="Ugho STEPHAN">
     <meta name="copyright" content="Ugho STEPHAN">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/icon">
+    
+    <!-- Let browser know website is optimized for mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">
+    <!-- Materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/css/materialize.min.css">
+    <!-- Animate -->
+    <link rel="stylesheet" href="css/animate.min.css">
+    <!-- App  -->
+    <link rel="stylesheet" href="css/app.min.css">
 
-    <meta charset="utf-8" />
-
-    <link rel="alternate" href="http://www.ugho-stephan.fr" hreflang="fr" />
   </head>
 
   <body>
