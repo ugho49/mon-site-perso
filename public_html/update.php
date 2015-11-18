ee547d099d89071baf1f50171df8c58be20c2f6f
Ugho STEPHAN <stephan.ugho@gmail.com>
2015-11-18 17:38:43 +0100

    Update update.php

--- public_html/update.php
+++ public_html/update.php
@@ -1,3 +1,6 @@
 <?php
-$output = shell_exec('cd /var/www/public_prod/ && git reset --hard origin/master 2>&1 && git pull 2>&1');
-var_dump($output);
+echo shell_exec('cd /var/www/public_prod/');
+echo '<br>';
+echo shell_exec('git reset --hard origin/master 2>&1');
+echo '<br>';
+echo shell_exec('git pull 2>&1');
