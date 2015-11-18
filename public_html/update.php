d6ada40ae15c0611465475cf9d2b29f211ff0271
Ugho STEPHAN <stephan.ugho@gmail.com>
2015-11-18 17:25:17 +0100

    Update update.php

--- public_html/update.php
+++ public_html/update.php
@@ -1,6 +1,6 @@
 <?php
-$output = shell_exec('cd /var/www/public_prod/ && git reset --hard origin/master 2>&1');
-$output = explode("\n", $output); 
+$sh = shell_exec('cd /var/www/public_prod/ && git reset --hard origin/master 2>&1');
+$output = explode("\n", $sh); 
 foreach($output as $line) 
 { 
     $line = rtrim($line); 
