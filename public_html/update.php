be5053e27eae1c4b5e33db5f8f1c89f1933be52e
Ugho STEPHAN <stephan.ugho@gmail.com>
2015-11-18 17:35:22 +0100

    Update update.php

--- public_html/update.php
+++ public_html/update.php
@@ -1,7 +1,3 @@
 <?php
-exec('cd /var/www/public_prod/ && git reset --hard origin/master 2>&1 && git pull 2>&1', $output);
-$scanme = implode("\n",$output);
-foreach($scanme as $line) 
-{ 
-    echo $line; 
-}  
+$output = shell_exec('cd /var/www/public_prod/ && git reset --hard origin/master 2>&1 && git pull 2>&1');
+var_dump($output);
