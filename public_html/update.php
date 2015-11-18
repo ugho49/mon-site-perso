5ba0933b21781cd3e83f1f577e7d5e11988db1c2
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-18 18:44:54 +0100

    ajout package.json from npm

--- public_html/update.php
+++ public_html/update.php
@@ -1,6 +1,5 @@
 <?php
-echo '<p>'.shell_exec('cd /var/www/public_prod/').'</p>';
-echo '<br>';
+shell_exec('cd /var/www/public_prod/');
 echo '<p>'.shell_exec('git reset --hard origin/master 2>&1').'</p>';
 echo '<br>';
 $output = shell_exec('git pull 2>&1');
