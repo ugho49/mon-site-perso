cdb45887e3118ca3cf8d6126e5cee589e069e4fe
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-18 18:50:30 +0100

    change update.php

--- public_html/update.php
+++ public_html/update.php
@@ -7,8 +7,6 @@ if(ereg("Already up-to-date.", $output)) {
     echo '<p style="color: green;">'.$output.'</p>';
 } else {
     echo '<p>'.$output.'</p>';
+    echo '<br>';
+    echo '<p>'.shell_exec('git reset --hard origin/master 2>&1').'</p>';
 }
-
-echo '<br>';
-
-echo '<p>'.shell_exec('git reset --hard origin/master 2>&1').'</p>';
