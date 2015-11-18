ddc1ff098d2cac299de1f3203457f54b1e9e5b08
Ugho Stephan <stephan.ugho@gmail.com>
2015-11-18 18:47:42 +0100

    materialize 0.97.2

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,10 +1,9 @@
-    
-    <!-- jQuery -->
+ <!-- jQuery -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <!-- Appear -->
     <script type="text/javascript" src="js/appear.min.js"></script>
     <!-- Materialize -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.2/js/materialize.min.js"></script>
     <!-- Init Materialize -->
     <script type="text/javascript" src="js/init.min.js"></script>
     <!-- App -->
@@ -20,10 +19,9 @@
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
           })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
-          ga('create', 'UA-48402317-4', 'auto');
+
+      ga('create', 'UA-48402317-4', 'auto');
           ga('send', 'pageview');
     </script>
-    
-  </body>
-</html>
+
+  </bod/html>
