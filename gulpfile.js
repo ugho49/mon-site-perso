537035c9a1bf4094f04e21422d28fde64a95c369
Ugho Stephan <stephan.ugho@gmail.com>
2016-04-28 20:23:03 +0200

    minor change for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,31 @@
-/*
-    npm install --global gulp
-*/
-
 // Load plugins
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    del = require('del');
-
-var dest_js  = "public"; // js folder create automaticly
-var dest_css = "public/css";
+    notify = require('gulp-notify');
 
 // Styles
 gulp.task('styles', function() {
-  return sass('assets/**/*.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(concat('style.min.css'))
-    .pipe(minifycss())
-    .pipe(gulp.dest(dest_css))
-    .pipe(notify({ message: 'Styles task complete' }));
+  return gulp.src('./assets/css/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(autoprefixer('last 2 version'))
+      .pipe(concat('style.min.css'))
+      .pipe(minifycss())
+      .pipe(gulp.dest('./public/css'))
+      .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('assets/**/*.js')
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest(dest_js))
-    .pipe(notify({ message: 'Scripts task complete' }));
+  return gulp.src('./assets/js/**/*.js')
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/js'))
+      .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Default task
@@ -44,9 +37,9 @@ gulp.task('default', function() {
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch('assets/css/**/*.scss', ['styles']);
+  gulp.watch('./assets/css/**/*.scss', ['styles']);
 
   // Watch .js files
-  gulp.watch('assets/js/**/*.js', ['scripts']);
+  gulp.watch('./assets/js/**/*.js', ['scripts']);
 
 });
