92ff2a1117e423066c91cad6b2e7e318f4b90e06
Ugho Stephan <stephan.ugho@gmail.com>
2017-01-08 19:30:15 +0100

    change languages icons

--- resources/views/pages/partials/header.blade.php
+++ resources/views/pages/partials/header.blade.php
@@ -1,8 +1,8 @@
 <!-- BEGIN Header -->
 <div class="parallax-container scrollspy" id="home">
     <div id="languages">
-        <img src="{{ URL::to('/build/images/icons/fr.jpg') }}" data-id="fr" data-url="{{ URL::route('changeLocale', 'fr') }}" class="language_flag @if(App::isLocale('fr')) active @endif">
-        <img src="{{ URL::to('/build/images/icons/en.jpg') }}" data-id="en" data-url="{{ URL::route('changeLocale', 'en') }}" class="language_flag @if(App::isLocale('en')) active @endif">
+        <img src="{{ URL::to('/build/images/icons/flags/France.png') }}" data-id="fr" data-url="{{ URL::route('changeLocale', 'fr') }}" class="language_flag @if(App::isLocale('fr')) active @endif">
+        <img src="{{ URL::to('/build/images/icons/flags/United-Kingdom.png') }}" data-id="en" data-url="{{ URL::route('changeLocale', 'en') }}" class="language_flag @if(App::isLocale('en')) active @endif">
     </div>
 
     <div class="headline">
